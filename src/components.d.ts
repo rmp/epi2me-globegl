3b35a9cc9b8aaa72872e03ff4b436ca6d058654c
Roger Pettett <rmp@psyphi.net>
2020-03-10 14:09:18 +0000

    initial import: working globegl-in-stencil; working GISAID preprocessor w/geocode.xyz lookup

--- src/components.d.ts
+++ src/components.d.ts
@@ -10,37 +10,31 @@ import { HTMLStencilElement, JSXBase } from '@stencil/core/internal';
 
 
 export namespace Components {
-  interface Epi2meDownload {
-    'downloads': string;
-    'platform': string;
-    'product': string;
-    'root': string;
+  interface Epi2meGlobegl {
+    'dataURI': string;
   }
 }
 
 declare global {
 
 
-  interface HTMLEpi2meDownloadElement extends Components.Epi2meDownload, HTMLStencilElement {}
-  var HTMLEpi2meDownloadElement: {
-    prototype: HTMLEpi2meDownloadElement;
-    new (): HTMLEpi2meDownloadElement;
+  interface HTMLEpi2meGlobeglElement extends Components.Epi2meGlobegl, HTMLStencilElement {}
+  var HTMLEpi2meGlobeglElement: {
+    prototype: HTMLEpi2meGlobeglElement;
+    new (): HTMLEpi2meGlobeglElement;
   };
   interface HTMLElementTagNameMap {
-    'epi2me-download': HTMLEpi2meDownloadElement;
+    'epi2me-globegl': HTMLEpi2meGlobeglElement;
   }
 }
 
 declare namespace LocalJSX {
-  interface Epi2meDownload {
-    'downloads'?: string;
-    'platform'?: string;
-    'product'?: string;
-    'root'?: string;
+  interface Epi2meGlobegl {
+    'dataURI'?: string;
   }
 
   interface IntrinsicElements {
-    'epi2me-download': Epi2meDownload;
+    'epi2me-globegl': Epi2meGlobegl;
   }
 }
 
@@ -50,7 +44,7 @@ export { LocalJSX as JSX };
 declare module "@stencil/core" {
   export namespace JSX {
     interface IntrinsicElements {
-      'epi2me-download': LocalJSX.Epi2meDownload & JSXBase.HTMLAttributes<HTMLEpi2meDownloadElement>;
+      'epi2me-globegl': LocalJSX.Epi2meGlobegl & JSXBase.HTMLAttributes<HTMLEpi2meGlobeglElement>;
     }
   }
 }
---
+++ src/components/epi2me-globegl/epi2me-globegl.tsx
@@ -0,0 +1,73 @@
+import { Component, Host, Prop, h } from '@stencil/core';
+import Globe from 'globe.gl';
+import { interpolateYlOrRd } from 'd3-scale-chromatic';
+//import { scaleSqrt } from 'd3-scale';
+//import { csvParse } from 'd3-dsv';
+
+@Component({
+  tag: 'epi2me-globegl',
+  styleUrl: 'epi2me-globegl.css'
+})
+
+export class EPI2MEGlobeGL {
+  @Prop() dataURI = '/data.json';
+
+  controller: any;
+  data: any;
+  config: any;
+  el: HTMLDivElement;
+
+  render() {
+    return (
+      <Host><div ref={el => { this.el = el as HTMLDivElement } }></div></Host>
+    );
+  }
+
+  componentWillLoad() {
+    const promises = [];
+
+    if (this.dataURI) {
+      promises.push(fetch(this.dataURI)
+        .then(response => response.json())
+        .then(data => {
+          this.data = data;
+        }));
+    }
+
+    return Promise.all(promises);
+  }
+
+  componentDidLoad() {
+    const weightColor = interpolateYlOrRd;
+
+    // Create Gio.controller
+    this.controller = Globe()
+
+    this.controller(this.el)
+      .globeImageUrl('//cdn.jsdelivr.net/npm/three-globe/example/img/earth-night.jpg')
+      .bumpImageUrl('//cdn.jsdelivr.net/npm/three-globe/example/img/earth-topology.png')
+      .backgroundImageUrl('//cdn.jsdelivr.net/npm/three-globe/example/img/night-sky.png')
+      .hexBinPointWeight('cnt')
+      .hexAltitude(d => d.sumWeight / 10) // fixed estimation of max data
+      .hexBinResolution(4)
+      .hexTopColor(d => weightColor(d.sumWeight))
+      .hexSideColor(d => weightColor(d.sumWeight))
+      .hexBinMerge(true)
+      .enablePointerInteraction(false); // performance improvement
+
+
+      this.controller.hexBinPointsData(this.data);
+
+  // Add auto-rotation
+  this.controller.controls().autoRotate = true;
+  this.controller.controls().autoRotateSpeed = 0.1;
+
+
+    // Add data
+//    this.controller.addData( this.data );
+
+    // Initialize and render the globe
+//    this.controller.init();
+//    this.controller.setAutoRotation( true, 0.2 );
+  }
+}
